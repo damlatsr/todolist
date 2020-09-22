ba5d5d09c65eb28ee5ade9d35f70a891e61f6fe9
damla <damlatsr@gmail.com>
2020-09-23 01:14:43 +0300

    added route to api.php.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::apiResource('todolist' , 'TaskController');
+Route::patch('todolist/done/{taskId}' , 'TaskController@done');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
